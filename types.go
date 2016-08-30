34460aa52068289c00cc09d60fc5654b9a11e189
Jon Wear <jon@dramafever.com>
2016-08-30 08:33:16 -0400

    update types, add peg brain that will, maybe not needed.

--- types.go
+++ types.go
@@ -1,46 +1,51 @@
 package tripeg
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
 
+//Hole struct that contains information
+//about a hole in the board, its location
+//and whether or not it has a peg in it.
 type Hole struct {
-	Index int
 	Row   int //make of 5
 	Col   int //max of 9
 	Peg   bool
 	Links []*Hole //Other Holes the hole is connected to
 }
 
+//Jump moves a peg from one hole to another
+//If it can jump, it removes the peg from the
+//overHole hole.
 func (h *Hole) Jump(overHole Hole) bool {
 	return false
 }
 
+//Board contains all the holes that contain the pegs
 type Board struct {
-	//May not need this
 	Holes []Hole
 }
 
 //BuildBoard makes a board of peg holes.
-//All holes are empty.  The top row has 1, then
+//All holes have a peg except one randomly assigned.
+//The top row has 1, then
 //2,3,4,5 for a total of 16 holes.
 func BuildBoard() Board {
 	var b Board
 	s2 := rand.NewSource(time.Now().UnixNano())
 	r2 := rand.New(s2)
-	fmt.Print(r2.Intn(16))
-	i := 0
+	empty := r2.Intn(16)
 	for r := 1; r < 6; r++ {
 		for c := 1; c < r+1; c++ {
 			col := 4 - (r) + (c * 2)
-			i++
-			h := Hole{Index: i, Row: r, Col: col}
+			h := Hole{Row: r, Col: col, Peg: true}
+			if empty == len(b.Holes) {
+				h.Peg = false
+			}
 			b.Holes = append(b.Holes, h)
 		}
 	}
-	fmt.Println("*", len(b.Holes))
 	return b
 }
 
