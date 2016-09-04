ead161714c545fbdd16440a67bc52f890112d5c7
Jon Wear <jon@dramafever.com>
2016-09-04 14:21:00 -0500

    getting the tests re-written

--- tri_test.go
+++ tri_test.go
@@ -1,53 +1,80 @@
 package tripeg
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestValidJumpVertical(t *testing.T) {
 	b := BuildBoard(1)
-	h := b.GetHole(3, 3)
-	o := b.GetHole(2, 4)
-	if h.Jump(b, o) != nil {
+	h, err1 := b.GetHole(3, 3)
+	if err1 != nil {
+		t.Errorf("Can't find hole 3,3")
+	}
+	o, err2 := b.GetHole(2, 4)
+	if err2 != nil {
+		t.Errorf("Can't find hole, 2,4")
+	}
+	_, err := b.Jump(h, o)
+	if err != nil {
 		t.Fatal("Should have been successful.")
 	}
 }
 
 func TestValidJumpHorizontal(t *testing.T) {
 	b := BuildBoard(6)
-	h := b.GetHole(3, 3)
-	o := b.GetHole(3, 5)
-	if h.Jump(b, o) != nil {
+	h, err1 := b.GetHole(3, 3)
+	if err1 != nil {
+		t.Errorf("Can't find hole 3,3")
+	}
+	o, err2 := b.GetHole(3, 5)
+	if err2 != nil {
+		t.Errorf("Can't find hole, 3,5")
+	}
+	_, err := b.Jump(h, o)
+	if err != nil {
 		t.Fatal("Should have been successful.")
 	}
 }
 
 func TestInvavidJumpOverHasNoPeg(t *testing.T) {
 	b := BuildBoard(6)
-	h := b.GetHole(3, 3)
-	o := b.GetHole(3, 5)
-	if h.Jump(b, o) != nil {
-		t.Fatal("Should have been successful.")
+	h, err1 := b.GetHole(2, 6)
+	if err1 != nil {
+		t.Errorf("Can't find hole 2,6")
 	}
-	h = b.GetHole(3, 7)
-	o = b.GetHole(3, 5)
-	if h.Jump(b, o) == nil {
-		t.Fatal("Should have failed.")
+	o, err2 := b.GetHole(3, 7)
+	if err2 != nil {
+		t.Errorf("Can't find hole, 3,7")
+	}
+	_, err := b.Jump(h, o)
+	if err != nil {
+		if !strings.HasPrefix(err.Error(), "No Peg in over hole") {
+			fmt.Println(err)
+			t.Fatal("Should have failed with No Peg in over hole.")
+		}
 	}
 }
 
 func TestInvavidTargetPegFull(t *testing.T) {
 	b := BuildBoard(6)
-	h := b.GetHole(4, 2)
-	o := b.GetHole(4, 4)
-	if h.Jump(b, o) == nil {
-		t.Fatal("Should have failed.")
+	h, err1 := b.GetHole(3, 3)
+	if err1 != nil {
+		t.Errorf("Can't find hole 3,3")
+	}
+	o, err2 := b.GetHole(2, 4)
+	if err2 != nil {
+		t.Errorf("Can't find hole, 2,4")
+	}
+	_, err := b.Jump(h, o)
+	if err != nil {
+		if err.Error() != "Target hole(1,5) has a peg in it\n" {
+			t.Fatal("Should have failed Target hole(1,5) has a peg in it")
+		}
 	}
 }
 
 func TestInvalidHolePegEmpty(t *testing.T) {
-	b := BuildBoard(6)
-	h := b.GetHole(3, 7)
-	o := b.GetHole(3, 5)
-	if h.Jump(b, o) == nil {
-		t.Fatal("Should have failed.")
-	}
+
 }
--- types.go
+++ types.go
@@ -11,15 +11,17 @@ import (
 //about a hole in the board, its location
 //and whether or not it has a peg in it.
 type Hole struct {
-	Row   int //make of 5
-	Col   int //max of 9
-	Peg   bool
-	Links []*Hole //Other Holes the hole is connected to
+	Row int //make of 5
+	Col int //max of 9
+	Peg bool
 }
 
 //Jump from the Board struct type
 func (b Board) Jump(m, o Hole) (Board, error) {
-	result := b
+	result := Board{}
+	for _, r := range b.Holes {
+		result.Holes = append(result.Holes, r)
+	}
 	if !m.Peg {
 		//If there is no peg in the moveHole, no jump possible
 		return result, fmt.Errorf("No Peg in move hole %d,%d\n", o.Row, o.Col)
@@ -76,22 +78,22 @@ func (b Board) Jump(m, o Hole) (Board, error) {
 		targetC = o.Col + x
 		//This is a jump right
 	}
-	targetHole := b.GetHole(targetR, targetC)
-	if targetHole == nil {
+	targetHole, err := b.GetHole(targetR, targetC)
+	if err != nil {
 		return result, fmt.Errorf("Target hole(%d,%d) does not exist\n", targetR, targetC)
 	}
 	if targetHole.Peg {
 		return result, fmt.Errorf("Target hole(%d,%d) has a peg in it\n", targetHole.Row, targetHole.Col)
 	}
-	for _, bh := range result.Holes {
+	for k, bh := range result.Holes {
 		if bh.Row == m.Row && bh.Col == m.Col {
-			bh.Peg = false
+			result.Holes[k].Peg = false
 		}
 		if bh.Row == o.Row && bh.Col == o.Col {
-			bh.Peg = false
+			result.Holes[k].Peg = false
 		}
 		if bh.Row == targetHole.Row && bh.Col == targetHole.Col {
-			bh.Peg = true
+			result.Holes[k].Peg = true
 		}
 	}
 	return result, nil
@@ -155,8 +157,8 @@ func (h *Hole) Jump(b Board, overHole *Hole) error {
 		targetC = overHole.Col + x
 		//This is a jump right
 	}
-	targetHole := b.GetHole(targetR, targetC)
-	if targetHole == nil {
+	targetHole, err := b.GetHole(targetR, targetC)
+	if err != nil {
 		return fmt.Errorf("Target hole(%d,%d) does not exist\n", targetR, targetC)
 	}
 	if targetHole.Peg {
@@ -170,21 +172,21 @@ func (h *Hole) Jump(b Board, overHole *Hole) error {
 
 //Board contains all the holes that contain the pegs
 type Board struct {
-	Holes   []*Hole
+	Holes   []Hole
 	MoveLog []string
 }
 
 //GetHole gets a pointer to a hole based on the row,col coordinates
-func (b Board) GetHole(r, c int) *Hole {
+func (b Board) GetHole(r, c int) (Hole, error) {
 	if r < 0 || r > 6 || c < 0 || c > 9 {
-		return nil
+		return Hole{}, fmt.Errorf("Hole %d,%d does not exist\n", r, c)
 	}
 	for _, v := range b.Holes {
 		if v.Col == c && v.Row == r {
-			return v
+			return v, nil
 		}
 	}
-	return nil
+	return Hole{}, fmt.Errorf("Hole %d,%d does not exist\n", r, c)
 }
 
 //BuildBoard makes a board of peg holes.
@@ -208,100 +210,23 @@ func BuildBoard(empty int) Board {
 			if empty == len(b.Holes) {
 				h.Peg = false
 			}
-			b.Holes = append(b.Holes, &h)
+			b.Holes = append(b.Holes, h)
 		}
 	}
 	return b
 }
 
 func (b *Board) Solve() {
-	b.MoveLog = []string{}
-	//Find out how many holes
-	//can make a legal Move
-	//find out how many moves can be made from those hole
-	//randomly pick one of those
-	//try again until there are no moves to make
-	//or 14 legal moves have been made, (winner)
-	//Print out all the winning moves
-	type cMove struct {
-		H *Hole
-		O *Hole
-	}
-	fboard := Board{}
-	fboard = *b
-	cMoves := []cMove{}
-	moves := 0
-	holes := b.Holes[0:2]
-	for _, v := range holes {
-		var bt = fboard
-		if v.Peg == true {
-			fmt.Println(v.Row, v.Col)
-			o := bt.GetHole(v.Row-1, v.Col-1)
-			if o != nil {
-				if v.Jump(bt, o) != nil {
-					//upleft
-					fmt.Println("UL")
-					cMoves = append(cMoves, cMove{H: v, O: o})
-				}
-			}
-			bt = fboard
-			o = bt.GetHole(v.Row-1, v.Col+1)
-			if o != nil {
-				if v.Jump(bt, o) != nil {
-					//upright
-					fmt.Println("UR")
-					cMoves = append(cMoves, cMove{H: v, O: o})
-				}
-			}
-			bt = fboard
-			o = bt.GetHole(v.Row, v.Col+2)
-			if o != nil {
-				if v.Jump(bt, o) != nil {
-					//right
-					cMoves = append(cMoves, cMove{H: v, O: o})
-				}
 
-			}
-			bt = fboard
-			o = bt.GetHole(v.Row, v.Col-2)
-			if o != nil {
-				if v.Jump(bt, o) != nil {
-					//left
-					cMoves = append(cMoves, cMove{H: v, O: o})
-				}
-			}
-			bt = fboard
-			o = bt.GetHole(v.Row+1, v.Col-2)
-			if o != nil {
-				if v.Jump(bt, o) != nil {
-					//downleft
-					cMoves = append(cMoves, cMove{H: v, O: o})
-				}
-			}
-			bt = fboard
-			o = bt.GetHole(v.Row+1, v.Col+2)
-			if o != nil {
-				if v.Jump(bt, o) != nil {
-					//downright
-					cMoves = append(cMoves, cMove{H: v, O: o})
-				}
-			}
-		}
-	}
-	_ = moves
-	for _, mv := range cMoves {
-		fmt.Println(mv.H, mv.O)
-	}
-	return
 }
 
 func (b Board) String() string {
 	result := "\n"
 	for r := 1; r < 6; r++ {
 		for c := 1; c < 10; c++ {
-			h := b.GetHole(r, c)
+			h, err := b.GetHole(r, c)
 			mark := " "
-			if h != nil {
+			if err == nil {
 				mark = "O"
 				if h.Peg {
 					mark = "*"
